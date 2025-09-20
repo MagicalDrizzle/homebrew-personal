f69d35f4e6092fdc744ba13a55c5e95d2ca72830
Mizuki Nguyen <49761211+MagicalDrizzle@users.noreply.github.com>
2025-09-20 05:40:12 +0000

    rate-mirrors: add 0.22.0 bottle.

--- Formula/rate-mirrors.rb
+++ Formula/rate-mirrors.rb
@@ -5,6 +5,13 @@ class RateMirrors < Formula
   sha256 "a0c5436ad51a5064755bcbf79f153ddcc16713637d2525c2bac8cbdcfc0443c5"
   license "CC-BY-NC-SA-3.0"
 
+  bottle do
+    root_url "https://github.com/MagicalDrizzle/homebrew-personal/releases/download/rate-mirrors-0.22.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "843e1a06eb87359976fccdf0b0af01ec49b0ffb48a86fd9bdd246ea20defe19a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "482f6072ae46459b545071435f6a4a34758450c0f9b9f7084dd9468884d6d1d1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "90d5fedf187e631f6d8f1360a357f78a5d79da37d691eec996e1b8b447422674"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3"
 

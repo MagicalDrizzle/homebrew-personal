bd52120583677dc83debe521e60e381fde2fd28d
Mizuki Nguyen <49761211+MagicalDrizzle@users.noreply.github.com>
2026-04-14 16:13:23 +0000

    Updated FFmpeg to 2026-04-14-13-18

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -2,7 +2,7 @@ class Ffmpeg < Formula
   desc "Linux, git master, GPL, shared builds of FFmpeg by BtbN"
   homepage "https://github.com/BtbN/FFmpeg-Builds"
   url "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl-shared.tar.xz"
-  version "2026-04-13-13-18"
+  version "2026-04-14-13-18"
   sha256 ""
   license "MIT"
 

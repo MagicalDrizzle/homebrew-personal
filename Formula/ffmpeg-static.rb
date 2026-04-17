c1c5dd8bb2fb96c84a2526dd2bba0b1378b2fed3
Mizuki Nguyen <49761211+MagicalDrizzle@users.noreply.github.com>
2026-04-17 16:05:13 +0000

    Updated FFmpeg to 2026-04-17-13-16

--- Formula/ffmpeg-static.rb
+++ Formula/ffmpeg-static.rb
@@ -2,7 +2,7 @@ class FfmpegStatic < Formula
   desc "Linux, git master, GPL, static builds of FFmpeg by BtbN"
   homepage "https://github.com/BtbN/FFmpeg-Builds"
   url "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz"
-  version "2026-04-16-13-18"
+  version "2026-04-17-13-16"
   sha256 ""
   license "MIT"
 
--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -2,7 +2,7 @@ class Ffmpeg < Formula
   desc "Linux, git master, GPL, shared builds of FFmpeg by BtbN"
   homepage "https://github.com/BtbN/FFmpeg-Builds"
   url "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl-shared.tar.xz"
-  version "2026-04-16-13-18"
+  version "2026-04-17-13-16"
   sha256 ""
   license "MIT"
 

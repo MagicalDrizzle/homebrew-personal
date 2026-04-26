71d0322317bb7e7a586016a86149cc1d085c26bd
Mizuki Nguyen <49761211+MagicalDrizzle@users.noreply.github.com>
2026-04-26 15:50:06 +0000

    Updated FFmpeg to 2026-04-26-13-08

--- Formula/ffmpeg-static.rb
+++ Formula/ffmpeg-static.rb
@@ -2,7 +2,7 @@ class FfmpegStatic < Formula
   desc "Linux, git master, GPL, static builds of FFmpeg by BtbN"
   homepage "https://github.com/BtbN/FFmpeg-Builds"
   url "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz"
-  version "2026-04-25-14-00"
+  version "2026-04-26-13-08"
   sha256 ""
   license "MIT"
 
